006d67c975c68c4d098de225c6cd81a48b1d8026
wjdgus3608 <wjdgus3608@naver.com>
2023-07-29 21:35:24 +0900

    SearchKeywordController 구현

--- keyword_news_project_server/src/main/java/com/example/server/controller/SearchKeywordController.java
+++ keyword_news_project_server/src/main/java/com/example/server/controller/SearchKeywordController.java
@@ -1,59 +1,67 @@
 package com.example.server.controller;
 
 import com.example.server.domain.searchkeyword.SearchKeyword;
+import com.example.server.domain.searchkeyword.dto.*;
 import com.example.server.service.SearchKeywordService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/keyword")
 public class SearchKeywordController {
     private final SearchKeywordService searchKeywordService;
 
-    public void addKeyword(){
-
+    @PostMapping("/addKeyword")
+    public ResponseEntity<?> addKeyword(@RequestBody @Valid AddKeywordDTO dto){
+        searchKeywordService.addKeyword(dto);
+        return ResponseEntity.ok().build();
     }
-
-    public void removeKeyword(){
-
+    @PostMapping("/removeKeyword")
+    public ResponseEntity<?> removeKeyword(@RequestBody @Valid FindKeywordDTO dto){
+        searchKeywordService.removeKeyword(dto);
+        return ResponseEntity.ok().build();
     }
-
-    public List<SearchKeyword> findAllKeyword(){
-
+    @PostMapping("/findAllKeyword/{ownerId}")
+    public List<SearchKeyword> findAllKeyword(@PathVariable String ownerId){
+        return searchKeywordService.findAllKeyword(ownerId);
     }
-
-    public Optional<SearchKeyword> findKeywordByInfo(){
-
+    @PostMapping("/findKeywordByInfo")
+    public Optional<SearchKeyword> findKeywordByInfo(@RequestBody @Valid FindKeywordDTO dto){
+        return searchKeywordService.findKeywordByInfo(dto);
     }
-
-    public void addContainKeyword(){
-
+    @PostMapping("/addContainKeyword")
+    public ResponseEntity<?> addContainKeyword(@RequestBody @Valid AddContainKeywordDTO dto){
+        searchKeywordService.addContainKeyword(dto.toFindDTO(), dto.getAddContainKeyword());
+        return ResponseEntity.ok().build();
     }
-
-    public void removeContainKeyword(){
-
+    @PostMapping("/removeContainKeyword")
+    public ResponseEntity<?> removeContainKeyword(@RequestBody @Valid RemoveContainKeywordDTO dto){
+        searchKeywordService.removeContainKeyword(dto.toFindDTO(), dto.getRemoveContainKeyword());
+        return ResponseEntity.ok().build();
     }
-
-    public List<String> findAllContainKeyword(){
-
+    @PostMapping("/findAllContainKeyword")
+    public List<String> findAllContainKeyword(@RequestBody @Valid FindKeywordDTO dto){
+        return searchKeywordService.findAllContainKeyword(dto);
     }
-
-    public void addExcludeKeyword(){
-
+    @PostMapping("/addExcludeKeyword")
+    public ResponseEntity<?> addExcludeKeyword(@RequestBody @Valid AddExcludeKeywordDTO dto){
+        searchKeywordService.addExcludeKeyword(dto.toFindDTO(), dto.getAddExcludeKeyword());
+        return ResponseEntity.ok().build();
     }
-
-    public void removeExcludeKeyword(){
-
+    @PostMapping("/removeExcludeKeyword")
+    public ResponseEntity<?> removeExcludeKeyword(@RequestBody @Valid RemoveExcludeKeywordDTO dto){
+        searchKeywordService.removeExcludeKeyword(dto.toFindDTO(), dto.getRemoveExcludeKeyword());
+        return ResponseEntity.ok().build();
     }
-
-    public List<String> findAllExcludeKeyword(){
-
+    @PostMapping("/findAllExcludeKeyword")
+    public List<String> findAllExcludeKeyword(@RequestBody @Valid FindKeywordDTO dto){
+        return searchKeywordService.findAllExcludeKeyword(dto);
     }
 
 
