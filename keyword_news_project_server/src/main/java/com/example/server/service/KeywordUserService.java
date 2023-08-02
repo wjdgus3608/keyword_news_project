e8ac232a2e0f183fa93fed4885c19967987642ff
wjdgus3608 <wjdgus3608@naver.com>
2023-08-02 23:16:51 +0900

    설정 업데이트 버그 수정

--- keyword_news_project_server/src/main/java/com/example/server/service/KeywordUserService.java
+++ keyword_news_project_server/src/main/java/com/example/server/service/KeywordUserService.java
@@ -4,13 +4,16 @@ import com.example.server.domain.keyworduser.KeywordUser;
 import com.example.server.domain.keyworduser.UpdateType;
 import com.example.server.domain.keyworduser.dto.UpdateSettingDTO;
 import com.example.server.repository.KeywordUserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class KeywordUserService {
     private final KeywordUserRepository keywordUserRepository;
 
@@ -41,10 +44,10 @@ public class KeywordUserService {
             user.get().setFcmToken(dto.getUpdateValue());
         }
         else if(dto.getUpdateType() == UpdateType.ALARM_ALLOWED){
-            user.get().setAlarmAllowed(Boolean.getBoolean(dto.getUpdateValue()));
+            user.get().setAlarmAllowed(Boolean.parseBoolean(dto.getUpdateValue()));
         }
         else if(dto.getUpdateType() == UpdateType.IS_VIP){
-            user.get().setVip(Boolean.getBoolean(dto.getUpdateValue()));
+            user.get().setVip(Boolean.parseBoolean(dto.getUpdateValue()));
         }
         else if(dto.getUpdateType() == UpdateType.FETCH_TIME){
             user.get().setFetchTime(dto.getUpdateValue());
