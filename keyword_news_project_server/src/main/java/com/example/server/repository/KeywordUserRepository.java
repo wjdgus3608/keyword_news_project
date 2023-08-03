bd017c99e5d17f48553dea7dbb86844d9ef7a27a
wjdgus3608 <wjdgus3608@naver.com>
2023-08-04 00:10:30 +0900

    다른아이디로 로그인시 기존아이디 active false 로 변경기능 추가

--- keyword_news_project_server/src/main/java/com/example/server/repository/KeywordUserRepository.java
+++ keyword_news_project_server/src/main/java/com/example/server/repository/KeywordUserRepository.java
@@ -3,5 +3,9 @@ package com.example.server.repository;
 import com.example.server.domain.keyworduser.KeywordUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface KeywordUserRepository extends CrudRepository<KeywordUser, String> {
+    List<KeywordUser> findKeywordUserByFcmToken(String fcmToken);
 }
